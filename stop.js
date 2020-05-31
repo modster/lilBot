6348ac36f71aa90f93980fcd3bb4e310ff866389
Mike Greeff <unmodster@gmail.com>
2020-05-31 04:32:01 -0600

    added new pine script

--- stop.js
+++
@@ -1,9 +0,0 @@
-
-eventEmitter.on('stop', () => {
-  binance.balance((error, balances) => {
-    if ( error ) return console.error(error);
-    let btcBalance = balances.BTC.available;
-    binance.marketSell(symbol, btcBalance);
-  }) // binance.balance
-}) // end eventemitter.on('stop')
-
